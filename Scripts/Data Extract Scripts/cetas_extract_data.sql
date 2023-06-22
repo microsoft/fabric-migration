9f9f9bbfdaab1ec7eda50371249891d835d39ea4
Pradeep Srikakolapu <prdpsvs@gmail.com>
2023-06-21 22:34:11 -0700

    Updated comments

--- Scripts/Data Extract Scripts/cetas_extract_data.sql
+++ Scripts/Data Extract Scripts/cetas_extract_data.sql
@@ -6,8 +6,8 @@ CREATE PROCEDURE dbo.sp_cetas_extract_script
     Name: dbo.sp_cetas_extract_script
     Description: This stored procedure uses system tables to generate CETAS script for extracting data to a data lake store
     Parameters: 
-        @adls_gen2_location - Base storage location of 
-        @storage_access_token - 
+        @adls_gen2_location - Base storage location of data stored in adls gen2 storage
+        @storage_access_token - Storage Account access key
 */
 AS
 
@@ -18,12 +18,12 @@ EXEC dbo.create_master_key_and_scoped_credential;
 EXEC dbo.create_external_file_format;
 
 -- Create External Data Source
-EXEC dbo.sp_create_external_data_source @adls_base_location = @external_data_source_base_location,
+EXEC dbo.sp_create_external_data_source @adls_base_location = @adls_gen2_location,
         @credential_name ='fabric_migration_credential',@external_data_source_name='fabric_data_migration_ext_data_source';
 
 -- Create a temporary table to hold the table data in dbo.table_info_for_data_extract;
 EXEC dbo.create_temp_table_view_to_extract_data;
 
 -- Generate data extract and data load statements
-EXEC dbo.create_temp_table_view_to_extract_data @storage_access_token = storage_access_token
-        , @external_data_source_base_location = @external_data_source_base_location
+EXEC dbo.create_temp_table_view_to_extract_data @storage_access_token = @storage_access_token
+        , @external_data_source_base_location = @adls_gen2_location
