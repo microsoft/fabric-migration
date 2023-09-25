cd5ef18526b9a506c0a1aae20e3710a82c805fad
Artur Vieira <arturv@microsoft.com>
2023-09-25 17:31:55 +0100

    DML Check update

--- Scripts/DDL Generate Scripts/Check_SynapseUnsupported.sql
+++ Scripts/DDL Generate Scripts/Check_SynapseUnsupported.sql
@@ -1,10 +1,9 @@
 --set nocount on
---DROP TABLE SQL_Unsupported01
-CREATE TABLE Synapse_Unsupported(ExprName varchar(200),ExprCode varchar(200))
---select * from Synapse_Unsupported
-
-INSERT INTO Synapse_Unsupported
-SELECT 'Triggers','TRIGGER 'UNION ALL 
+IF (OBJECT_ID(N'TempDB..#Synapse_Unsupported','U') IS NOT NULL)
+DROP TABLE #Synapse_Unsupported
+GO
+CREATE TABLE #Synapse_Unsupported with(distribution=Round_Robin,heap) AS
+SELECT 'Triggers' ExprName,'TRIGGER ' ExprCode UNION ALL 
 SELECT 'XML',' XML 'UNION ALL 
 SELECT 'JSON',' JSON 'UNION ALL 
 SELECT 'MATCH',' MATCH'UNION ALL 
@@ -37,19 +36,25 @@ SELECT 'Geometry Data Type',' GEOMETRY 'UNION ALL
 SELECT 'Geography Data Type',' GEOGRAPHY 'UNION ALL 
 SELECT 'filestream',' FILESTREAM 'UNION ALL 
 SELECT 'FileTable',' FILETABLE'UNION ALL 
-SELECT 'Memory Optimized Tables','MEMORY_OPTIMIZED'UNION ALL 
-SELECT 'Full Text Search','FULLTEXT'UNION ALL 
+SELECT 'Memory Optimized Tables','MEMORY_OPTIMIZED 'UNION ALL 
+SELECT 'Full Text Search','FULLTEXT 'UNION ALL 
+SELECT 'Table Definition','HASH'UNION ALL 
+SELECT 'Table Definition','REPLICATE'UNION ALL 
+SELECT 'Table Definition','ROUND_ROBIN'UNION ALL 
+SELECT 'Table Definition','PARTITION'UNION ALL 
+SELECT 'Table Definition','COLUMNSTORE INDEX'UNION ALL 
+SELECT 'Table Definition','CLUSTERED INDEX'UNION ALL 
+SELECT 'Table Definition','NONCLUSTERED INDEX'UNION ALL 
+SELECT 'Table Definition',' IDENTITY'UNION ALL 
 SELECT 'Row Count','ROWCOUNT'
---ADD checks for: Distribution, Partition, indexes
 
 GO
 --Create Procedure CheckForUnsupportedCode as
-SELECT ss.[name], o.[name],x.[value],c.ExprName,c.ExprCode
+SELECT ss.[name] SchemaName, o.[name] SPName,x.[value] CodeToReview,c.ExprName CodeCategory,c.ExprCode CodeKeyWord
   FROM sys.sql_modules AS sm
-  INNER JOIN sys.objects AS o  ON sm.object_id = o.object_id  
-  INNER JOIN sys.schemas AS ss ON o.schema_id = ss.schema_id   
-  CROSS APPLY STRING_SPLIT(sm.[definition], char(13)) x
-  INNER JOIN Synapse_Unsupported c on upper(x.value) like Upper(concat('%',c.ExprCode,'%'))
-WHERE o.type='P' and ss.[name] NOT IN ('INFORMATION_SCHEMA','sys','sysdiag','migration')
-group by ss.[name], o.[name],x.[value],c.ExprName,c.ExprCode
-
+ INNER JOIN sys.objects AS o  ON sm.object_id = o.object_id  
+ INNER JOIN sys.schemas AS ss ON o.schema_id = ss.schema_id   
+ CROSS APPLY STRING_SPLIT(sm.[definition], char(13)) x
+ INNER JOIN #Synapse_Unsupported c ON UPPER(x.value) like Upper(concat('%',c.ExprCode,'%'))
+ WHERE o.type='P' AND ss.[name] NOT IN ('INFORMATION_SCHEMA','sys','sysdiag','migration')
+ GROUP BY ss.[name], o.[name],x.[value],c.ExprName,c.ExprCode
